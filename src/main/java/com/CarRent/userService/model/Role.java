de70c672bf99ae20ce5fcbb18cd4a8ea2f7e2c1f
HP <aleksa.prokic888@gmail.com>
2022-12-23 18:50:02 +0100

    Initial commit, DB setup ; user, role

---
+++ src/main/java/com/CarRent/userService/model/Role.java
@@ -0,0 +1,29 @@
+package com.CarRent.userService.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+    public Role() {
+    }
+
+    public Role(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+}
---
+++ src/main/java/com/CarRent/userService/model/User.java
@@ -0,0 +1,39 @@
+package com.CarRent.userService.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(indexes = {@Index(columnList = "username", unique = true), @Index(columnList = "passportNumber", unique = true)})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //all users
+    private String username;
+    private String password;
+    private String phoneNumber;
+    private Date birthDate;
+    private String firstName;
+    private String lastName;
+
+    @ManyToOne
+    private Role role;
+
+    //client
+    private String passportNumber;
+    private Long rentDaysNumber;
+    private Boolean restricted;
+
+    //manager
+    private String companyName;
+    private Date hireDate;
+
+}
